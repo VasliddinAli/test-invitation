8d5c7575308f9027cc99223206f2d69ba60655c8
Vasliddin M. Alijonov <v.alijonov@uzairports.com>
2025-11-19 18:01:12 +0500

    ...

--- datas.js
+++ datas.js
@@ -11,7 +11,7 @@ let bazm_date = document.querySelector(".bazm_date"); // 14:00
 let wedding_start_date = document.querySelector(".wedding_start_date"); // 2025-yil 12-oktyabr
 let map_iframe = document.querySelector(".map_iframe"); // <iframe>......
 
-const start_date = "2025-10-12";
+const start_date = "2025-11-28";
 
 var d = new Date(new Date(start_date).getTime());
 simplyCountdown(".simply-countdown-one", {
@@ -29,18 +29,18 @@ function SetDate(date) {
 var c = new Cal("divCal");
 c.showcurr();
 
-boy_name.innerHTML = "Husanboy";
-girl_name.innerHTML = "Hilolaxon";
-boy_name_start_letter.innerHTML = "H";
-girl_name_start_letter.innerHTML = "H";
-address_invitation.innerHTML = "12-oktyabr, 2025 Farg'ona, Quva tumani";
+boy_name.innerHTML = "Muslimbek";
+girl_name.innerHTML = "Azizaxon";
+boy_name_start_letter.innerHTML = "M";
+girl_name_start_letter.innerHTML = "A";
+address_invitation.innerHTML = "28-noyabr, 2025 Farg'ona, Quva tumani";
 contact_address.innerHTML =
-  "Farg'ona viloyati, Quva tumani, Shodlik to'yxonasi.";
-contact_moljal.innerHTML = "Qorashox shaxarchasi, 7-maktab yonida";
-contact_phone.innerHTML = "+99850 6666 456";
-contact_phone.setAttribute("href", "tel:+99850 6666 456");
-naxor_date.innerHTML = "09:00";
-bazm_date.innerHTML = "14:00";
-wedding_start_date.innerHTML = "2025-yil 12-oktyabr";
+  "Shaxrisabz shaxri, Shukrona to'yxonasi.";
+contact_moljal.innerHTML = "Shukrona to'yxonasi";
+contact_phone.innerHTML = "+998946 1111 00";
+contact_phone.setAttribute("href", "tel:+998946 1111 00");
+naxor_date.innerHTML = "15:00";
+bazm_date.innerHTML = "15:00";
+wedding_start_date.innerHTML = "2025-yil 28-noyabr";
 
-map_iframe.innerHTML = `<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d379.0249711813067!2d72.01639050445164!3d40.53717542778157!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2s!4v1757048800517!5m2!1sen!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`;
+map_iframe.innerHTML = `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d918.6811375824827!2d66.8310369696303!3d39.06315413953398!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3f4c9bb3fca5cfc3%3A0x5d8be061134b6c8f!2sSHUKRONA!5e1!3m2!1sen!2s!4v1763557128686!5m2!1sen!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`;
